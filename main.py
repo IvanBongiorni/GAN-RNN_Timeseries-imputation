1c4206a86451319b3dfd0ca030bc4eeffecf767c
ivan <ivanbongiorni@gmail.com>
2020-04-05 23:36:53 +0200

    minor fix

--- main.py
+++ main.py
@@ -16,34 +16,34 @@ def run():
     import numpy as np
     import pandas as pd
     import tensorflow as tf
-    
+
     # local modules
     import processing, model, train
-    
+
     # Get current directory
     current_path = os.getcwd()
-    
+
     # Load configuration params
     params = yaml.load(open(current_path + '/config.yaml'), yaml.Loader)
-    
+
     # Update params with path data
     params['data_path'] = current_path + '/data/'
     params['save_path'] = current_path + '/saved_models/'
-    
+
     # Load data
     X, V, Y = processing.load_and_process_data(params)
-    
+
     # Load and train model
     if params['gan']:
-        Generator, Adversary = model.build_GAN(params)
-        train.train_GAN(Generator, Adversary, X, V, params)
+        Generator, Discriminator = model.build_GAN(params)
+        train.train_GAN(Generator, Discriminator, X, V, params)
     else:
         CRNN = model.build(params)
         train.train(CRNN, X, V, params)
-    
+
     # Check performance on Test data
     chech_performance_on_test_data(Y)
-    
+
     return None
 
 
