b683019529bce7b645e8d4b82bde60cc33ad2813
IvanBongiorni <ivanbongiorni@gmail.com>
2020-05-02 00:53:19 +0200

    major reorganization due to OOM error. Moving part of preprocessing to train

--- processing.py
+++ processing.py
@@ -7,27 +7,62 @@ Data preprocessing pipeline. Separated from model implementation and training.
 import tools
 
 
-def get_page_language(df):
-    """
-    Attaches page data to main df by iterating process_url() from
-    dataprep_tools.py:
-        language:  code - with 'na' for 'no language detected'
-        website:   what type of website: 'wikipedia', 'wikimedia', 'mediawiki'
-        access:    type of access (e.g.: mobile, desktop, both, ...)
-        agent:     type of agent
-    """
+# def apply_processing_transformations(array, params, scaling_percentile):
+#     '''
+#     Wrapper of the main data transformations. Since they had to be repeated on
+#     Train, Validation, Test sets, I packed a funtion to avoid repetitions.
+#     '''
+#     import numpy as np
+#     import tools  # local module
+#
+#     # Scale trends
+#     array = tools.scale_trends(array, scaling_percentile)
+#
+#     # Fill left-NaN's with zeros
+#     array = [ tools.left_zero_fill(array[ i , : ]) for i in range(array.shape[0]) ]
+#
+#     # Trim right-NaN's
+#     array = [ tools.right_trim_nan(series) for series in array ]
+#
+#     # Exclude trends that still contain internal NaN's
+#     array = [ series for series in array if np.sum(np.isnan(series)) == 0 ]
+#
+#     # Exclude trends that are not long enough to be fed into the series
+#     array = [ series for series in array if len(series) >= params['len_input'] ]
+#
+#     # Process to RNN format ('sliding window' to input series) and pack into final array
+#     array = [ tools.RNN_univariate_processing(series, len_input = params['len_input']) for series in array ]
+#
+#     array = np.concatenate(array)
+#     return array
+
+
+def assemble_dataframe(languages, data):
+    '''
+    This function is due to memory problems, not all dataframes can be loaded in
+    RAM at the same time.
+
+    Loads all preprocessed sub-dataframes and compacts them into one final array.
+    It must repeated three times, for Train, Validation and Test matrices.
+    Arg 'data' must be either 'train', 'val', or 'test'
+    '''
+    import os
+    import pickle
     import numpy as np
-    import pandas as pd
-    import tools  # local module
 
-    page_lang = [ tools.process_url(url) for url in df['Page'].tolist() ]
-    page_lang = pd.Series(page_data)
+    X_final = []
 
-    df.drop('Page', axis = 1, inplace = True)
-    return df, page_lang
+    for language in languages:
+        sdf_lang = np.load(os.getcwd() + '/data_processed/sdf_{}_{}.npy'.format(language, data),
+                           allow_pickle = True)
+        X_final.append(sdf_lang)
+        del sdf_lang
+
+    X_final = np.concatenate(X_final)
+    return X_final
 
 
-def main(params):
+def processing_main():
     '''
     Main wrapper of the input pipe. Steps:
     1. Loads pandas dataframe, takes values only and converts to np.array
@@ -37,31 +72,33 @@ def main(params):
     4. Shuffles dataset and operates Train-Validation-Test split based on params
     '''
     import os
+    import time
+    import yaml
     import pickle
     import numpy as np
-    np.random.seed(params['seed'])
+    import pandas as pd
 
     languages = [ 'en', 'ja', 'de', 'fr', 'zh', 'ru', 'es', 'na' ]
 
-    print('Loading data.')
-    current_path = os.getcwd()
-    df = pd.read_csv(current_path + '/data/train2.csv')
+    print('\nStart data processing pipeline.\n')
+    print('Loading data and configuration parameters.')
+    df = pd.read_csv(os.getcwd() + '/data_raw/train_2.csv')
+    params = yaml.load(open(os.getcwd() + '/config.yaml'), yaml.Loader)
 
     print('Extracting URL metadata from dataframe.')
-    df, page_lang = get_page_language(df)
+    page_lang = [ tools.process_url(url) for url in df['Page'].tolist() ]
+    page_lang = pd.Series(page_lang)
+    df.drop('Page', axis = 1, inplace = True)
 
     print('Preprocessing trends by language group.')
-    X_train = []
-    X_val = []
-    X_test = []
+    np.random.seed(params['seed'])
 
     scaling_dict = {}   # This is to save scaling params - by language subgroup
 
     for language in languages:
         start = time.time()
 
-        sdf = df[ page_lang['language'] == language ].values
-        sdf_lang = page_lang[ page_lang['language'] == language ].values
+        sdf = df[ page_lang == language ].values
 
         ### SPLIT IN TRAIN - VAL - TEST
         # Generate random index to each row following 'val_test_size' Pr distribution
@@ -72,67 +109,60 @@ def main(params):
         sdf_train = sdf[ sample == 0 ]
         sdf_val = sdf[ sample == 1 ]
         sdf_test = sdf[ sample == 2 ]
+        del sdf # free memory
+
+        ### I MUST TRANSFER PROCESSING PIPELINE ON SCRATCH, DURING TRAINING
 
         # Scale and save param into dict
-        scaling_percentile = np.nanpercentile(sdf_train, 99)  # np.nanpercentile excludes NaN's from computation
-        sdf_train = scale_trends(sdf, scaling_percentile)
-        sdf_val = scale_trends(sdf_val, scaling_percentile)
-        sdf_test = scale_trends(sdf_val, scaling_percentile)
-        scaling_dict[language] = scaling_percentile
+        scaling_percentile = np.nanpercentile(sdf_train, 99)  # np.nanpercentile ignores NaN's
+        scaling_dict[language] = float(scaling_percentile)
 
-        # Fill left-NaN's with zeros
-        sdf_train = [ tools.left_zero_fill(sdf_train[ i , : ]) for i in range(sdf_train.shape[0]) ]
-        sdf_val = [ tools.left_zero_fill(sdf_val[ i , : ]) for i in range(sdf_val.shape[0]) ]
-        sdf_test = [ tools.left_zero_fill(sdf_test[ i , : ]) for i in range(sdf_test.shape[0]) ]
+        # Apply sequence of processing transformations, save to folder, and del sdf's to free memory
+        # sdf_train = apply_processing_transformations(sdf_train, params, scaling_percentile)
+        # np.save(os.getcwd() + '/data_processed/sdf_{}_train'.format(language), sdf_train)
+        # del sdf_train
 
-        # Trim right-NaN's
-        sdf_train = [ tools.right_trim_nan(series, params) for series in sdf_train ]
-        sdf_val = [ tools.right_trim_nan(series, params) for series in sdf_val ]
-        sdf_test = [ tools.right_trim_nan(series, params) for series in sdf_test ]
+        # sdf_val = apply_processing_transformations(sdf_val, params, scaling_percentile)
+        # np.save(os.getcwd() + '/data_processed/sdf_{}_val'.format(language), sdf_val)
+        # del sdf_val
 
-        # Exclude trends that still contain internal NaN's
-        sdf_train = [ series for series in sdf_train if np.sum(np.isnan(series)) = 0 ]
-        sdf_val = [ series for series in sdf_val if np.sum(np.isnan(series)) = 0 ]
-        sdf_test = [ series for series in sdf_test if np.sum(np.isnan(series)) = 0 ]
+        # sdf_test = apply_processing_transformations(sdf_test, params, scaling_percentile)
+        # np.save(os.getcwd() + '/data_processed/sdf_{}_test'.format(language), sdf_test)
+        # del sdf_test
 
-        # Exclude trends that are not long enough to be fed into the series
-        sdf_train = [ series for series in sdf_train if len(series) >= params['len_input'] ]
-        sdf_val = [ series for series in sdf_val if len(series) >= params['len_input'] ]
-        sdf_test = [ series for series in sdf_test if len(series) >= params['len_input'] ]
-
-        # Process to RNN format ('sliding window' to input series) and pack into final array
-        sdf_train = [ tools.RNN_univariate_processing(series, params) for series in sdf_train ]
-        sdf_train = np.concatenate(sdf_train)
-
-        sdf_val = [ tools.RNN_univariate_processing(series, params) for series in sdf_val ]
-        sdf_val = np.concatenate(sdf_val)
-
-        sdf_test = [ tools.RNN_univariate_processing(series, params) for series in sdf_test ]
-        sdf_test = np.concatenate(sdf_test)
-
-        X_train.append(sdf_train)
-        X_val.append(sdf_val)
-        X_test.append(sdf_test)
-
-        print("\tSub-dataframe for language '{}' executed in {} ss.".format(language, round(time.time()-start, 2)))
-
-    X_train = np.concatenate(X_train)
-    X_val = np.concatenate(X_val)
-    X_test = np.concatenate(X_test)
-    
-    # shuffle X_train for batch training
-    shuffle = np.random.choice(X.shape[0], X.shape[0] replace = False)
-    X_train = X_train[ shuffle , : ]
-
-    pickle.dump(X_train, open( os.getcwd() + '/data_processed/X_train.pkl' ))
-    pickle.dump(X_val, open( os.getcwd() + '/data_processed/X_train.pkl' ))
-    pickle.dump(X_test, open(os.getcwd() + '/data_processed/X_val.pkl'))
+        print("\tSub-dataframe for language '{}' executed in {} ss.".format(
+            language, round(time.time()-start, 2)))
 
     # Save scaling params to file
     yaml.dump(scaling_dict, open( os.getcwd() + '/data_processed/scaling_dict.yaml', 'w'))
 
+    # print('Assembling Train, Validation, Test datasets.')
+    # # Assemble and save datasets, free memory, delete temporary sdf files
+    # X_train = assemble_dataframe(languages, 'train')
+    #
+    # Shuffle X_train only before training
+    # shuffle = np.random.choice(X_train.shape[0], X_train.shape[0], replace = False)
+    # X_train = X_train[ shuffle , : ]
+    # np.save(os.getcwd() + '/data_processed/X_train', X_train)
+    # del X_train
+    # for language in languages:
+    #     os.remove(os.getcwd() + '/data_processed/sdf_{}_train.npy'.format(language))
+    #
+    # X_val = assemble_dataframe(languages, 'val')
+    # np.save(os.getcwd() + '/data_processed/X_val', X_val)
+    # del X_val
+    # for language in languages:
+    #     os.remove(os.getcwd() + '/data_processed/sdf_{}_train.npy'.format(language))
+    #
+    # X_test = assemble_dataframe(languages, 'test')
+    # np.save(os.getcwd() + '/data_processed/X_test', X_test)
+    # del X_test
+    # for language in languages:
+    #     os.remove(os.getcwd() + '/data_processed/sdf_{}_test.npy'.format(language))
+
+    print('Processed datasets saved at:\n{}'.format(os.getcwd() + '/data_processed/'))
     return None
 
 
 if __name__ == '__main__':
-    main()
+    processing_main()
--- tools.py
+++ tools.py
@@ -5,11 +5,12 @@ Author: Ivan Bongiorni
 Tools for data processing pipeline. These are more technical functions to be iterated during
 main pipeline run.
 """
-import numba
+# import numba
 
 
-@numba.jit(python = True)
+# @numba.jit(python = True)
 def left_zero_fill(x):
+    import numpy as np
     if np.isfinite(x[0]):
         return x
 
@@ -35,8 +36,8 @@ def process_url(url):
     return lang
 
 
-@numba.jit(python = True)
-def scale_trends(x, percentile_99th):
+# @numba.jit(python = True)
+def scale_trends(x, scaling_percentile):
     """
     Takes a linguistic sub-dataframe and applies a robust custom scaling in two steps:
         1. log( x + 1 )
@@ -45,16 +46,12 @@ def scale_trends(x, percentile_99th):
     """
     import numpy as np
 
-    # Scaling parameters must be calculated without Validation data
-    cut = int(x.shape[1] * (1 - (params['val_test_size'][0] + params['val_test_size'][1])))
-    percentile_99th = np.percentile(X[ : , :cut ]), 99)
-
     x = np.log(x + 1)
-    x = ( x - np.min(x) ) / ( percentile_99th - np.min(x) )
-    return X
+    x = x / scaling_percentile
+    return x
 
 
-@numba.jit(python = True)
+# @numba.jit(python = True)
 def right_trim_nan(x):
     """ Trims all NaN's on the right """
     import numpy as np
@@ -66,21 +63,16 @@ def right_trim_nan(x):
         return x
 
 
-def RNN_univariate_processing(series, params):
+# @numba.jit(python = True)
+def RNN_univariate_processing(series, len_input):
     ''' From 1D series creates 2D matrix of sequences defined by params['len_input'] '''
+    # This function is a simplification of RNN_dataprep from:
     # https://github.com/IvanBongiorni/TensorFlow2.0_Notebooks/blob/master/TensorFlow2.0__04.02_RNN_many2many.ipynb
     import numpy as np
+    import numpy as np
+    
+    S = [ series[i : i+len_input] for i in range(len(series)-len_input+1) ]
+    S = np.stack(S)
 
-    S = [ series[ i : i + params['len_input'] + len_pred] for i in range(S.shape[0]) ]
-    S = np.concatenate(S)
-
-    train = S[: , :len_input]
-    test = S[: , -len_pred:]
-
-    # reshape data as required by Keras LSTM
-    train = train.reshape((len(train), len_input, 1))
-    test = test.reshape((len(test), len_pred))
-
-    train = train.astype(np.float32)
-    test = test.astype(np.float32)
-    return train, test
+    S = S.astype(np.float32)
+    return S
