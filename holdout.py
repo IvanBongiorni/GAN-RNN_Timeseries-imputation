830d8cd751bee855ad9e37068d05fd204375b58a
IvanBongiorni <ivanbongiorni@gmail.com>
2020-06-22 15:14:36 +0200

    added gpu config when called from terminal

--- holdout.py
+++ holdout.py
@@ -35,7 +35,7 @@ def check_performance(model, path, params):
         batch = np.load(path+file)
         X_batch, Y_batch = train.process_series(batch, params)
         p = model.predict(X_batch)
-        X.append(x)
+        X.append(X_batch)
         Y.append(Y_batch)
         P.append(p)
 
@@ -143,6 +143,35 @@ if __name__ == '__main__':
     import tensorflow as tf
 
     params = yaml.load( open(os.getcwd() + '/config.yaml'), yaml.Loader )
+
+    print('Setting GPU configurations.')
+    ### Sets GPU configurations
+    if params['use_gpu']:
+        # This prevents CuDNN 'Failed to get convolution algorithm' error
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Memory growth must be set before GPUs have been initialized
+                print(e)
+        # To see list of allocated tensors in case of OOM
+        tf.compat.v1.RunOptions(report_tensor_allocations_upon_oom = True)
+    else:
+        try:
+            # Disable all GPUs
+            tf.config.set_visible_devices([], 'GPU')
+            visible_devices = tf.config.get_visible_devices()
+            for device in visible_devices:
+                assert device.device_type != 'GPU'
+        except:
+            print('Invalid device or cannot modify virtual devices once initialized.')
+        pass
+
     model = tf.keras.models.load_model(os.getcwd() + '/saved_models/' + params['model_name'] + '.h5')
 
     run_test(model, params)
