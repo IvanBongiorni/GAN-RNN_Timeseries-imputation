c04d2616584c41c94da8274fa568f4968300e4f2
IvanBongiorni <ivanbongiorni@gmail.com>
2020-07-28 21:25:46 +0200

    completed multivariate training of p-GAN

--- train.py
+++ train.py
@@ -39,6 +39,9 @@ def process_series(batch, params):
 
 
 def train_vanilla_seq2seq(model, params):
+    '''
+    ## TODO:  [ doc to be rewritten ]
+    '''
     import time
     import numpy as np
     import tensorflow as tf
@@ -56,8 +59,6 @@ def train_vanilla_seq2seq(model, params):
         gradients = tape.gradient(current_loss, model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
         return current_loss
-
-    ## Training session starts
 
     # Get list of all Training and Validation observations
     X_files = os.listdir( os.getcwd() + '/data_processed/Training/' )
@@ -107,12 +108,15 @@ def train_vanilla_seq2seq(model, params):
 
 
 def train_GAN(generator, discriminator, params):
+    '''
+    ## TODO:  [ doc to be rewritten ]
+    '''
     import time
     import numpy as np
     import tensorflow as tf
 
     cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True) # this works for both G and D
-    MAE = tf.keras.losses.MeanAbsoluteError()  # to check Validation performance
+    # MAE = tf.keras.losses.MeanAbsoluteError()  # to check Validation performance
 
     generator_optimizer = tf.keras.optimizers.Adam(learning_rate=params['learning_rate'])
     discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=params['learning_rate'])
@@ -121,12 +125,7 @@ def train_GAN(generator, discriminator, params):
     def generator_loss(discriminator_guess_fakes):
         return cross_entropy(tf.ones_like(discriminator_guess_fakes), discriminator_guess_fakes)
 
-    # @tf.function
-    # def discriminator_loss(discriminator_guess_reals, discriminator_guess_fakes):
-    #     loss_fakes = cross_entropy(tf.zeros_like(discriminator_guess_fakes), discriminator_guess_fakes)
-    #     loss_real = cross_entropy(tf.ones_like(discriminator_guess_reals), discriminator_guess_reals)
-    #     return loss_fakes + loss_real
-
+    # Label smoothing
     @tf.function
     def discriminator_loss(discriminator_guess_reals, discriminator_guess_fakes):
         loss_fakes = cross_entropy(
@@ -139,7 +138,7 @@ def train_GAN(generator, discriminator, params):
         return loss_fakes + loss_reals
 
     @tf.function
-    def train_step(X_batch, real_example, mask):
+    def train_step(X_batch, real_example):
         with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
 
             generator_imputation = generator(X_batch)
@@ -184,8 +183,6 @@ def train_GAN(generator, discriminator, params):
             # fetch batch by filenames index and train
             batch = np.load( '{}/data_processed/Training/{}'.format(os.getcwd(), X_files[iteration]) )
             X_batch, Y_batch, mask = process_series(batch, params)
-
-            # BP()
 
             # Load another series of real observations ( this block is a subset of process_series() )
             real_example = np.load( '{}/data_processed/Training/{}'.format(os.getcwd(),  np.random.choice(np.delete(X_files, iteration))))
@@ -241,128 +238,123 @@ def train_GAN(generator, discriminator, params):
     return None
 
 
-# def train_partial_GAN(generator, discriminator, params):
-#     '''
-#     Modification of train_GAN(), with the addition of MAE Loss for Generator.
-#     Now Generator's Loss is composed of two elements: a canonical regression Loss
-#     (MAE), and he GAN Loss obtained from trying to fool the Discriminator. Since
-#     their magnitude are quite different, a balance in the final loss values must be
-#     achieved through artificial weighting, determined by params['loss_weight'].
-#     '''
-#     import time
-#     import numpy as np
-#     import tensorflow as tf
-#
-#     cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True) # this works for both G and D
-#     MAE = tf.keras.losses.MeanAbsoluteError()  # to check Validation performance
-#
-#     generator_optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
-#     discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
-#
-#     @tf.function
-#     def generator_loss(batch, deteriorated, discriminator_guess_fakes, w):
-#         mae_loss = MAE(batch, deteriorated)
-#         gan_loss = cross_entropy(tf.ones_like(discriminator_guess_fakes), discriminator_guess_fakes)
-#
-#         generator_loss = mae_loss * w + gan_loss * (1-w)
-#         return generator_loss
-#
-#     @tf.function
-#     def discriminator_loss(discriminator_guess_reals, discriminator_guess_fakes):
-#         loss_fakes = cross_entropy(tf.zeros_like(discriminator_guess_fakes), discriminator_guess_fakes)
-#         loss_reals = cross_entropy(tf.ones_like(discriminator_guess_reals), discriminator_guess_reals)
-#
-#         tf.print('Discriminator Loss, fakes, reals:', loss_fakes, loss_reals)
-#
-#         return loss_fakes + loss_reals
-#
-#     @tf.function
-#     def train_step(batch, deteriorated, real_example, w):
-#         with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
-#
-#             generator_imputation = generator(deteriorated)
-#
-#             discriminator_guess_fakes = discriminator(generator_imputation)
-#             discriminator_guess_reals = discriminator(real_example)
-#
-#             generator_current_loss = generator_loss(batch, deteriorated, discriminator_guess_fakes, w)
-#             discriminator_current_loss = discriminator_loss(discriminator_guess_reals, discriminator_guess_fakes)
-#
-#         generator_gradient = generator_tape.gradient(generator_current_loss, generator.trainable_variables)
-#         dicriminator_gradient = discriminator_tape.gradient(discriminator_current_loss, discriminator.trainable_variables)
-#
-#         generator_optimizer.apply_gradients(zip(generator_gradient, generator.trainable_variables))
-#         discriminator_optimizer.apply_gradients(zip(dicriminator_gradient, discriminator.trainable_variables))
-#
-#         return generator_current_loss, discriminator_current_loss
-#
-#     # Get list of all Training and Validation observations
-#     X_files = os.listdir( os.getcwd() + '/data_processed/Training/' )
-#     if 'readme_training.md' in X_files: X_files.remove('readme_training.md')
-#     if '.gitignore' in X_files: X_files.remove('.gitignore')
-#     X_files = np.array(X_files)
-#
-#     V_files = os.listdir( os.getcwd() + '/data_processed/Validation/' )
-#     if 'readme_validation.md' in V_files: V_files.remove('readme_validation.md')
-#     if '.gitignore' in V_files: V_files.remove('.gitignore')
-#     V_files = np.array(V_files)
-#
-#     for epoch in range(params['n_epochs']):
-#
-#         # Shuffle data by shuffling row index
-#         if params['shuffle']:
-#             X_files = X_files[ np.random.choice(X_files.shape[0], X_files.shape[0], replace = False) ]
-#
-#         for iteration in range(X_files.shape[0]):
-#         # for iteration in range( int(X_files.shape[0] * 0.1) ):      ### TEMPORARY TEST
-#             start = time.time()
-#
-#             # fetch batch by filenames index and train
-#             batch = np.load( '{}/data_processed/Training/{}'.format(os.getcwd(), X_files[iteration]) )
-#             batch, deteriorated = process_series(batch, params)
-#
-#             # Load another series of real observations ( this block is a subset of process_series() )
-#             real_example = np.load( '{}/data_processed/Training/{}'.format(os.getcwd(), np.random.choice(np.delete(X_files, iteration))))
-#             real_example = real_example[ np.isfinite(real_example) ] # only right-trim NaN's. Others were removed in processing
-#             real_example = tools.RNN_univariate_processing(real_example, len_input = params['len_input'])
-#             sample = np.random.choice(real_example.shape[0], size = np.min([real_example.shape[0], params['batch_size']]), replace = False)
-#             real_example = real_example[ sample , : ]
-#             real_example = np.expand_dims(real_example, axis = -1)
-#
-#             generator_current_loss, discriminator_current_loss = train_step(batch, deteriorated, real_example, params['loss_weight'])
-#
-#             if iteration % 100 == 0:
-#                 # To get Generative and Aversarial Losses (and binary accuracy)
-#                 generator_imputation = generator(deteriorated)
-#                 discriminator_guess_reals = discriminator(real_example)
-#                 discriminator_guess_fakes = discriminator(generator_imputation)
-#
-#                 # Check Imputer's plain Loss on training example
-#                 train_loss = MAE(batch, generator(deteriorated))
-#
-#                 # Add imputation Loss on Validation data
-#                 v_file = np.random.choice(V_files)
-#                 batch = np.load( '{}/data_processed/Validation/{}'.format(os.getcwd(), v_file) )
-#                 batch, deteriorated = process_series(batch, params)
-#                 val_loss = MAE(batch, generator(deteriorated))
-#
-#                 print('{}.{}   \tGenerator Loss: {}   \tDiscriminator Loss: {}   \tDiscriminator Accuracy (reals, fakes): ({}, {})   \tTime: {}ss'.format(
-#                     epoch, iteration,
-#                     generator_current_loss,
-#                     discriminator_current_loss,
-#                     tf.reduce_mean(tf.keras.metrics.binary_accuracy(tf.ones_like(discriminator_guess_reals), discriminator_guess_reals)),
-#                     tf.reduce_mean(tf.keras.metrics.binary_accuracy(tf.zeros_like(discriminator_guess_fakes), discriminator_guess_fakes)),
-#                     round(time.time()-start, 4)
-#                 ))
-#                 print('\t\tImputation Loss: {}   \tValidation Loss: {}\n'.format(train_loss, val_loss))
-#
-#     print('\nTraining complete.\n')
-#
-#     generator.save('{}/saved_models/{}.h5'.format(os.getcwd(), params['model_name']))
-#     print('Generator saved at:\n{}'.format('{}/saved_models/{}.h5'.format(os.getcwd(), params['model_name'])))
-#
-#     if params['save_discriminator']:
-#         discriminator.save('{}/saved_models/{}_discriminator.h5'.format(os.getcwd(), params['model_name']))
-#         print('\nDiscriminator saved at:\n{}'.format('{}/saved_models/{}_discriminator.h5'.format(os.getcwd(), params['model_name'])))
-#
-#     return None
+def train_partial_GAN(generator, discriminator, params):
+    '''
+    ## TODO:  [ doc to be rewritten ]
+    '''
+    import time
+    import numpy as np
+    import tensorflow as tf
+
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True) # this works for both G and D
+    MAE = tf.keras.losses.MeanAbsoluteError()  # to check Validation performance
+
+    generator_optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
+    discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
+
+    @tf.function
+    def train_step(X_batch, Y_batch, real_example, mask, w):
+        with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
+
+            generator_imputation = generator(X_batch)
+            # that's complex: it prepares an imputed batch for the Discriminator. It removes the first
+            # variable (at [:,:,0]) that is the deteriorated trend, and puts the imputation made by the Generator
+            generator_imputation = tf.concat([ generator_imputation, X_batch[:,:,1:] ], axis=-1)
+
+            discriminator_guess_fakes = discriminator(generator_imputation)
+            discriminator_guess_reals = discriminator(real_example)
+
+            # Generator loss
+            mask = tf.expand_dims(mask, axis=-1)
+            g_loss_mae = tf.reduce_mean(tf.math.abs(
+                tf.math.multiply(generator(X_batch), mask) - tf.math.multiply(Y_batch, mask)))
+            g_loss_gan = cross_entropy(tf.ones_like(discriminator_guess_fakes), discriminator_guess_fakes)
+            generator_current_loss = g_loss_mae + (g_loss_gan * w)  # magnitude of GAN loss to be adjusted
+
+            # Disccriminator loss
+            d_loss_fakes = cross_entropy(tf.zeros_like(discriminator_guess_fakes), discriminator_guess_fakes)
+            d_loss_reals = cross_entropy(tf.ones_like(discriminator_guess_reals), discriminator_guess_reals)
+            discriminator_current_loss = cross_entropy(tf.zeros_like(discriminator_guess_fakes), discriminator_guess_fakes) + cross_entropy(tf.ones_like(discriminator_guess_reals), discriminator_guess_reals)
+
+        generator_gradient = generator_tape.gradient(generator_current_loss, generator.trainable_variables)
+        dicriminator_gradient = discriminator_tape.gradient(discriminator_current_loss, discriminator.trainable_variables)
+
+        generator_optimizer.apply_gradients(zip(generator_gradient, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(dicriminator_gradient, discriminator.trainable_variables))
+
+        return generator_current_loss, discriminator_current_loss
+
+    # Get list of all Training and Validation observations
+    X_files = os.listdir( os.getcwd() + '/data_processed/Training/' )
+    if 'readme_training.md' in X_files: X_files.remove('readme_training.md')
+    if '.gitignore' in X_files: X_files.remove('.gitignore')
+    X_files = np.array(X_files)
+
+    V_files = os.listdir( os.getcwd() + '/data_processed/Validation/' )
+    if 'readme_validation.md' in V_files: V_files.remove('readme_validation.md')
+    if '.gitignore' in V_files: V_files.remove('.gitignore')
+    V_files = np.array(V_files)
+
+    for epoch in range(params['n_epochs']):
+
+        # Shuffle data by shuffling row index
+        if params['shuffle']:
+            X_files = X_files[ np.random.choice(X_files.shape[0], X_files.shape[0], replace = False) ]
+
+        for iteration in range(X_files.shape[0]):
+        # for iteration in range( int(X_files.shape[0] * 0.1) ):      ### TEMPORARY TEST
+            start = time.time()
+
+            # fetch batch by filenames index and train
+            batch = np.load( '{}/data_processed/Training/{}'.format(os.getcwd(), X_files[iteration]) )
+            X_batch, Y_batch, mask = process_series(batch, params)
+
+            # Load another series of real observations ( this block is a subset of process_series() )
+            real_example = np.load( '{}/data_processed/Training/{}'.format(os.getcwd(), np.random.choice(np.delete(X_files, iteration))))
+            real_example = tools.RNN_multivariate_processing(real_example, len_input = params['len_input'])
+            sample = np.random.choice(real_example.shape[0], size = np.min([real_example.shape[0], params['batch_size']]), replace = False)
+            real_example = real_example[ sample , : ]
+
+            ### TODO: UNIFORMARE QUESTI INPUT CON LA FUNZIONE SOPRA
+            generator_current_loss, discriminator_current_loss = train_step(X_batch, Y_batch, real_example, mask, params['loss_weight'])
+
+            if iteration % 100 == 0:
+                # To get Generative and Aversarial Losses (and binary accuracy)
+                # for Generator simply repeat what's in train_step() above
+                generator_imputation = generator(X_batch)
+                generator_imputation = tf.concat([ generator_imputation, X_batch[:,:,1:] ], axis=-1)
+                discriminator_guess_reals = discriminator(real_example)
+                discriminator_guess_fakes = discriminator(generator_imputation)
+
+                # Check Imputer's plain Loss on training example
+                # train_loss = MAE(batch, generator(deteriorated))
+                train_loss = tf.reduce_mean(tf.math.abs(
+                    tf.math.multiply(generator(X_batch), tf.expand_dims(mask, axis=-1)) - tf.math.multiply(Y_batch, tf.expand_dims(mask, axis=-1))))
+
+                # Add imputation Loss on Validation data
+                v_file = np.random.choice(V_files)
+                batch = np.load( '{}/data_processed/Validation/{}'.format(os.getcwd(), v_file) )
+                X_batch, Y_batch, mask = process_series(batch, params)
+                val_loss = tf.reduce_mean(tf.math.abs(
+                    tf.math.multiply(generator(X_batch), tf.expand_dims(mask, axis=-1)) - tf.math.multiply(Y_batch, tf.expand_dims(mask, axis=-1))))
+
+                print('{}.{}   \tGenerator Loss: {}   \tDiscriminator Loss: {}   \tDiscriminator Accuracy (reals, fakes): ({}, {})   \tTime: {}ss'.format(
+                    epoch, iteration,
+                    generator_current_loss,
+                    discriminator_current_loss,
+                    tf.reduce_mean(tf.keras.metrics.binary_accuracy(tf.ones_like(discriminator_guess_reals), discriminator_guess_reals)),
+                    tf.reduce_mean(tf.keras.metrics.binary_accuracy(tf.zeros_like(discriminator_guess_fakes), discriminator_guess_fakes)),
+                    round(time.time()-start, 4)
+                ))
+                print('\t\tImputation Loss: {}   \tValidation Loss: {}\n'.format(train_loss, val_loss))
+
+    print('\nTraining complete.\n')
+
+    generator.save('{}/saved_models/{}.h5'.format(os.getcwd(), params['model_name']))
+    print('Generator saved at:\n{}'.format('{}/saved_models/{}.h5'.format(os.getcwd(), params['model_name'])))
+
+    if params['save_discriminator']:
+        discriminator.save('{}/saved_models/{}_discriminator.h5'.format(os.getcwd(), params['model_name']))
+        print('\nDiscriminator saved at:\n{}'.format('{}/saved_models/{}_discriminator.h5'.format(os.getcwd(), params['model_name'])))
+
+    return None
